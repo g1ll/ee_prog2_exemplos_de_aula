6c41fb6295c69ea87028188a4818d43fdda4522f
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-05 16:25:33 -0300

    refact: remocao exemplos extras

--- aula-03-arvores/arvoreint.cpp
+++ aula-03-arvores/arvoreint.cpp
@@ -9,34 +9,16 @@ int main()
 
 	ArvoreBinariadeBusca<int> btree;
 
-	// btree.inserir(10);
 	btree.inserir(4);
 	btree.inserir(2);
 	btree.inserir(6);
-	// btree.inserir(15);
-	// btree.inserir(20);
-	// btree.inserir(18);
 	btree.inserir(1);
 	btree.inserir(3);
 	btree.inserir(5);
 	btree.inserir(7);
-	// btree.inserir(19);
-	// btree.inserir(13);
-	// btree.inserir(8);
-	// btree.inserir(0);
-	// btree.inserir(14);
-	// btree.inserir(17);
-	// btree.inserir(16);
-	// btree.inserir(12);
 
 	cout << "Imprime em ordem: " << endl;
 	btree.imprimeInOrden(nullptr);
-
-	cout << "Imprime em pre-ordem: " << endl;
-	btree.imprimePreOrden(nullptr);
-
-	cout << "Imprime em pos-ordem: " << endl;
-	btree.imprimePosOrden(nullptr);
 
 	int n;
 	cout << "Buscar um numero: ";
@@ -56,9 +38,4 @@ int main()
 	cout << "Imprime em ordem: " << endl;
 	btree.imprimeInOrden(nullptr);
 
-	cout << "Imprime em pre-ordem: " << endl;
-	btree.imprimePreOrden(nullptr);
-
-	cout << "Imprime em pos-ordem: " << endl;
-	btree.imprimePosOrden(nullptr);
 }
