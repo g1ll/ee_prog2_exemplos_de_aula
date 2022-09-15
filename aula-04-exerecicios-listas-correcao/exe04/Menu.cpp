6de715bbf3140504e0404236e0633704ad4d7353
Prof. Gill <gillvelleda@gmail.com>
2022-09-14 21:20:26 -0300

    reaproveitando o metodo anterior entradaValor

--- aula-04-exerecicios-listas-correcao/exe04/Menu.cpp
+++ aula-04-exerecicios-listas-correcao/exe04/Menu.cpp
@@ -19,15 +19,5 @@ int Menu::entradaValor(string message, int start, int end)
 
 void Menu::entradaValor(string message, int start, int end, int &opt)
 {
-    cout << "\n"
-         << message;
-    cin >> opt;
-    while (cin.fail() || opt < start || opt > end)
-    {
-        cin.clear();
-        cin.ignore(1000, '\n');
-        cout << "Opcoes invalida! Tente novamente "
-             << start << "-" << end << endl;
-        cin >> opt;
-    }
+    opt = entradaValor(message,start,end,opt)
 }
